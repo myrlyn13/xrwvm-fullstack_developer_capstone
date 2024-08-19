fc497790556889d7c7366092a66d643081cd719c
Myranda <myranda.lynne13@gmail.com>
2024-08-19 02:26:13 -0400

    Adding temporary changes to Github

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,34 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealerships = await Dealerships.find({});
+        res.json(dealerships);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const state = req.params.state;
+        const dealerships = await Dealerships.find({ state: state });
+        res.json(dealerships);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const id = req.params.id;
+        const dealerships = await Dealerships.find({ id: id });
+        res.json(dealerships);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 //Express route to insert review
