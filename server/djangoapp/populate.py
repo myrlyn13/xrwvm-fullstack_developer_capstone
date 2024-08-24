fa1600e700edc7d200e16e27c8c2b247ebd57344
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-23 20:21:15 -0400

    populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -19,7 +19,7 @@ def initiate ():
 {"name": "Qashqai", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
 {"name": "XTRAIL", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
 {"name": "A-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
-{"name": "C-Class", "type": SUV", "year": 2023, "car_make": car_make_instances[1]},
+{"name": "C-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
 {"name": "E-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
 {"name": "A4", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
 {"name": "A5", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
