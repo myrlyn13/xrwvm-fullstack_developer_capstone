551c468e0108674a69847cde41375a4de1e9e3f7
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-25 01:34:06 -0400

    settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -100,26 +100,26 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': (
-    'django.contrib.auth.password_validation.'
-        'UserAttributeSimilarityValidator',
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator',
         ),
     },
     {
         'NAME': (
-    'django.contrib.auth.password_validation.'
+            'django.contrib.auth.password_validation.'
             'MinimumLengthValidator',
         ),
     },
     {
         'NAME': (
-    'django.contrib.auth.password_validation.'
+            'django.contrib.auth.password_validation.'
             'CommonPasswordValidator',
         ),
     },
     {
         'NAME': (
-    'django.contrib.auth.password_validation.'
-        'NumericPasswordValidator',
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator',
         ),
     },
 ]
