dcffdb7c5c369088b7ff8023bf20c8d49d8d6816
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-25 01:48:41 -0400

    restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,7 +10,7 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-def get_request(endpoint, **kwargs):
+ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
@@ -24,7 +24,7 @@ def get_request(endpoint, **kwargs):
                 print(f"Unexpected {err=}, {type(err)=}")
                 print("Network exception occurred")
 
-def analyze_review_sentiments(text):
+ def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         response = requests.get(request_url)
@@ -32,8 +32,7 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
-        
-def post_review(data_dict):
+ def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
