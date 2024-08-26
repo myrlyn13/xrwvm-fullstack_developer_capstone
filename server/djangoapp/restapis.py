a4d25f713a16ff68c2b4a413dc965df95538175a
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-26 15:23:01 -0400

    restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,7 +10,7 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
- def get_request(endpoint, **kwargs):
+def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
