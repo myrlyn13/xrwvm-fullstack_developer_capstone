f19be332aa4d0b146b6dfffa87e3d6664b713080
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-28 22:49:24 -0400

    restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -20,7 +20,7 @@ def get_request(endpoint, **kwargs):
             try:
                 response = requests.get(request_url)
                 return response.json()
-            except Exception as err:
+            except Exception as e:
                 print(f"Unexpected {err=}, {type(err)=}")
                 print("Network exception occurred")
 
@@ -38,6 +38,6 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except Exception as err:
+    except Exception as e:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
