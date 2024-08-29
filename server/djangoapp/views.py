50ad27defdcb89d2bc19c155ee215fa6d98ad077
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-29 01:24:54 -0400

    views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -36,7 +36,7 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-def logout_user(request):
+def logout_request(request):
     logout(request)
     data = {"userName": ""}
     return JsonResponse(data)
@@ -44,7 +44,7 @@ def logout_user(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -52,19 +52,25 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    # email_taken = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except Exception as err:
         # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        logger.debug("{} is new user".format(username) + e)
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email,
+        )
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -79,10 +85,10 @@ def registration(request):
 # particular state if state is passed
 
 def get_dealerships(request, state= "All"):
-    if (state == "All"):
+    if state == "All":
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
@@ -90,8 +96,8 @@ def get_dealerships(request, state= "All"):
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if (dealer_id):
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
@@ -104,8 +110,8 @@ def get_dealer_reviews(request, dealer_id):
 # Create a `get_dealer_details` view to render the dealer details
      
 def get_dealer_details(request, dealer_id):
-    if (dealer_id):
-        endpoint = "/fetchDealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
@@ -114,29 +120,26 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
          
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as err:
-            return JsonResponse({
-                "status": 401, "message": "Error in posting review"
-            })
+            return JsonResponse({"status": 401, "message": "Error"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 # Get list of cars 
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if (count == 0):
+    if count == 0:
         initiate()
         car_models = CarModel.objects.select_related('car_make')
         cars = []
-        for car_model in car_models:
-            cars.append({
-                "CarModel": car_model.name, 
-                "CarMake": car_model.car_make.name
-            })
-            return JsonResponse({"CarModels": cars})
-            return JsonResponse({"CarModels": cars})
+        
+    for car_model in car_models:
+        cars.append(
+            {"CarModel": car_model.name,"CarMake": car_model.car_make.name}
+        )
+    return JsonResponse({"CarModels": cars})
