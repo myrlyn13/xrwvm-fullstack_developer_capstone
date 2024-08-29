24dee770bee1008d49aca0fba9cd4a33e36dbae9
myrlyn13 <myranda.lynne13@gmail.com>
2024-08-29 03:02:47 -0400

    views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -124,8 +124,8 @@ def get_cars(request):
         initiate()
         car_models = CarModel.objects.select_related('car_make')
         cars = []
-    for car_model in car_models:
-        cars.append(
-            {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
-        )
+for car_model in car_models:
+    cars.append(
+        {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
+    )
     return JsonResponse({"CarModels": cars})
