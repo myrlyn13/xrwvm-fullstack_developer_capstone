0075fac62c0f585d35ca6eddd836ca0f1cfde9e3
Myranda <myranda.lynne13@gmail.com>
2024-10-27 19:37:47 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,12 +2,24 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 # Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = [
+        "name",
+        "type",
+        "year",
+    ]
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = [
+        "name"
+        "description",
+    ]
+    inlines = [CarModelInline]
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
